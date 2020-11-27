939263bc68f48052927445720c6853cc4a9037e6
Yu Tang <yutang@YuTang-MBP.local>
2020-11-27 16:01:11 +0800

    Updates

--- gallery/src/components/Gallery.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="gallery">
-    <div class="gallery-panel"
-         v-for="photo in photos"
-         :key="photo.id">
-      <img :src="thumbUrl(photo.filename)">
-    </div>
-  </div>
-</template>
